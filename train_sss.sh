6f96e13c0b473d3835a46ca9a7f7142613e085e3
Zhijie Wang <mynameiswzj@gmail.com>
2022-06-23 17:53:39 +0900

    remove some redundant scripts

--- train_sss.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-#$-pe gpu 4
-#$-l gpu=4
-#$-j y
-#$-cwd
-#$-V
-
-export CUDA_VISIBLE_DEVICES=$SGE_GPU
-bash tools/dist_train.sh configs/mobilenet_v2/${config}.py 4 --work-dir work_dirs/${config}/
